08767b73819f42ba2a2e05dd900863474d8e4c0d
shinji-yoshida <yoshida.shinji.gm@gmail.com>
2012-09-08 14:32:03 +0900

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Analyswf::Application.config.secret_token = '1d2476ba3d9a3c7f8e9eb49cb800d74cbc4852bb6b8d3ef30861ca1d507da71cad5ebbfc5ffcc513f5da2e1dd19dd97ffdc2d95c6a13acd8d47ff9bd67653ad7'
