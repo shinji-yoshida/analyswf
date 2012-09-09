33522713e12556df5a53568a6bb2ba0062de6fae
shinji-yoshida <yoshida.shinji.gm@gmail.com>
2012-09-10 00:26:23 +0900

    swf_series: new action でターゲットを保存できるようにした

--- app/controllers/swf_series_controller.rb
+++ app/controllers/swf_series_controller.rb
@@ -13,4 +13,25 @@ class SwfSeriesController < ApplicationController
   def edit
     @series = SwfSeries.find(params[:id])
   end
+
+  def create
+    @series = SwfSeries.new(params[:swf_series])
+    targets = {}
+    p params
+    params.each do |key, value|
+      unless key.match(/^target_/)
+        next
+      end
+      targets[key] = value
+    end
+    targets.each do |key, type|
+      @series.put_target(key.sub(/^target_/, ''), type)
+    end
+
+    if @series.save
+      redirect_to @series, notice: 'Post was successfully created.'
+    else
+      render action: "new"
+    end
+  end
 end
