e8670ef3305b7e43ddd8a0ba332d58f7c6f6fd73
shinji-yoshida <yoshida.shinji.gm@gmail.com>
2012-09-09 16:44:52 +0900

    swf_series/_form でターゲットを追加できるようにした

--- app/models/target_type.rb
+++ app/models/target_type.rb
@@ -1,7 +1,15 @@
 module TargetType
-  JPEG = 'jpeg'
-  GIF = 'gif'
-  MULTI_LINE_TEXT = 'multi_line_text'
-  ONE_LINE_TEXT = 'one_line_text'
-  CONTROL =  'control'
+  TYPES = {
+    jpeg: 'jpeg',
+    gif: 'gif',
+    multi_line_text: 'multi_line_text',
+    one_line_text: 'one_line_text',
+    control: 'control'
+  }
+
+  JPEG = TYPES[:jpeg]
+  GIF = TYPES[:gif]
+  MULTI_LINE_TEXT = TYPES[:multi_line_text]
+  ONE_LINE_TEXT = TYPES[:one_line_text]
+  CONTROL =  TYPES[:control]
 end
