389e8c1baf5bf3af62054e9855ecd09f0e8b8f80
shinji-yoshida <yoshida.shinji.gm@gmail.com>
2012-09-16 20:15:27 +0900

    与えられた swf の画像差し替え用offsetを分析・保存できるようにした

--- app/controllers/swfs_controller.rb
+++ app/controllers/swfs_controller.rb
@@ -1,3 +1,7 @@
+# -*- encoding: utf-8 -*-
+require 'fileutils'
+require 'swf_analyzer'
+
 class SwfsController < ApplicationController
   def index
     @swf_series = SwfSeries.find(params[:swf_series_id])
@@ -18,11 +22,53 @@ class SwfsController < ApplicationController
 
   def create
     swf_series = SwfSeries.find(params[:swf_series_id])
-    f = params[:file]
     Swf.transaction do
-      @swf = swf_series.swfs.create!
-      @swf.create_swf_binary!(data: f.read)
+      @swf = create_swf(swf_series, params[:file].read)
     end
     redirect_to action: :show, id: @swf.id
+  end
+
+  private
+
+  def create_swf(swf_series, swf_data)
+    swf = swf_series.swfs.build
+    analyzed = analyze_swf(swf_data)
+    analyzed.jpeg_offsets.each do |o|
+      swf.offsets.build(offset_type: TargetType::JPEG, value: o)
+    end
+    analyzed.gif_offsets.each do |o|
+      swf.offsets.build(offset_type: TargetType::GIF, value: o)
+    end
+    swf.build_swf_binary(data: swf_data)
+    swf.save!
+    return swf
+  end
+
+  def tmp_swf_dir
+    result = Rails.root.join('tmp', 'swf')
+    FileUtils.mkdir_p result
+    return result
+  end
+
+  def tmp_swf_file
+    tmp_swf_file = File.join(tmp_swf_dir, 'tmp.swf')
+  end
+
+  def analyze_swf(data)
+    do_analyze_swf(data)
+  rescue => e
+    delete_tmp_swf_file
+    raise ApplicationException.new "例外: #{e.message}"
+  end
+
+  def do_analyze_swf(data)
+    File.binwrite(tmp_swf_file, data)
+    result = SwfAnalyzer.analyze_swf(tmp_swf_file)
+    delete_tmp_swf_file
+    return result
+  end
+
+  def delete_tmp_swf_file
+    File.delete(tmp_swf_file)
   end
 end
--- spec/controllers/swfs_controller_spec.rb
+++ spec/controllers/swfs_controller_spec.rb
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 require 'spec_helper'
 
 describe SwfsController do
@@ -29,6 +30,43 @@ describe SwfsController do
     it "returns http success" do
       get 'edit'
       response.should be_success
+    end
+  end
+
+  describe 'create_swf' do
+    before do
+      @swf_data = 'swf data'
+      controller.stub(:analyze_swf).and_return stub(jpeg_offsets: [1, 3], gif_offsets: [2, 5])
+      controller.send(:create_swf, swf_series, @swf_data)
+      swf_series.reload
+    end
+
+    it 'jpeg タイプのオフセットとして 1 をもつ' do
+      swf_series.swfs.first.offsets.should be_any{|o| o.offset_type == TargetType::JPEG && o.value == 1}
+    end
+
+    it 'jpeg タイプのオフセットとして 3 をもつ' do
+      swf_series.swfs.first.offsets.should be_any{|o| o.offset_type == TargetType::JPEG && o.value == 3}
+    end
+
+    it 'gif タイプのオフセットとして 2 をもつ' do
+      swf_series.swfs.first.offsets.should be_any{|o| o.offset_type == TargetType::GIF && o.value == 2}
+    end
+
+    it 'jpeg タイプのオフセットとして 5 をもつ' do
+      swf_series.swfs.first.offsets.should be_any{|o| o.offset_type == TargetType::GIF && o.value == 5}
+    end
+
+    it 'swf_binary を生成する' do
+      swf_series.swfs.first.swf_binary.data.should == @swf_data
+    end
+  end
+
+  describe 'analyze_swf' do
+    it '例外発生時には一時ファイルを削除する' do
+      controller.stub(:do_analyze_swf).and_throw('exception')
+      controller.should_receive(:delete_tmp_swf_file)
+      expect{ controller.send(:analyze_swf, 'data')}.to raise_error
     end
   end
 
