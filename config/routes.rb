8c730eae51de4db3dda2077c7730505cd9c374a8
shinji-yoshida <yoshida.shinji.gm@gmail.com>
2012-09-17 15:24:13 +0900

    トップページ変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Analyswf::Application.routes.draw do
-
+  root :to => 'swf_series#index'
   resources :swf_series
   resources :swfs
   resources :swf_binaries do
@@ -64,5 +64,5 @@ Analyswf::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  match ':controller(/:action(/:id))(.:format)'
+  # match ':controller(/:action(/:id))(.:format)'
 end
