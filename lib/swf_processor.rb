1cfec943d35eab5cd3d1ec97b9cb5a0e9bbdbefd
shinji-yoshida <yoshida.shinji.gm@gmail.com>
2012-09-22 22:03:08 +0900

    swf_processor 作成

---
+++ lib/swf_processor.rb
@@ -0,0 +1,17 @@
+require 'swf_ruby'
+
+class SwfProcessor
+  attr_accessor :swf_binary
+
+  def initialize(swf_binary)
+    @swf_binary = swf_binary
+  end
+
+  def replace(swf_parameters)
+    replace_images(swf_parameters)
+  end
+
+  def replace_images(image_swf_parameters)
+    SwfRuby::SwfTamperer.replace(swf_binary, image_swf_parameters.map(&:create_replace_target))
+  end
+end
