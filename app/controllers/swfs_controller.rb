c43aa42612a8a5f2353c5cf4423399a008544521
shinji-yoshida <yoshida.shinji.gm@gmail.com>
2012-09-23 14:48:38 +0900

    swf: 画像ターゲットに offset を割り当てられるようにした

--- app/controllers/swfs_controller.rb
+++ app/controllers/swfs_controller.rb
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
 require 'fileutils'
+require 'hash_extension'
 require 'swf_analyzer'
 require 'swf_parameter'
 
@@ -12,6 +13,8 @@ class SwfsController < ApplicationController
   def show
     @swf = Swf.find(params[:id])
     @swf_series = @swf.swf_series
+    image_target_names = @swf_series.jpeg_target_names + @swf_series.gif_target_names
+    @target_name_to_offset_map = create_target_name_to_offset_map(image_target_names, @swf.offsets)
   end
 
   def new
@@ -31,11 +34,35 @@ class SwfsController < ApplicationController
     redirect_to action: :show, id: @swf.id
   end
 
+  def update
+    swf = Swf.find(params[:id])
+    new_offsets = decode_prefixed(params, 'offset_')
+    Swf.transaction do
+      swf.offsets.each do |o|
+        o.target_name = new_offsets[o.value.to_s] # could be nil
+        o.save!
+      end
+    end
+    redirect_to swf_url(id: swf.id)
+  end
+
   def test
     parameters = SwfParameter.decode_params(params)
   end
 
   private
+
+  def create_target_name_to_offset_map(target_names, offsets)
+    target_names.inject({}) do |result, name|
+      offset = offsets.find{|o| o.target_name == name}
+      result[name] = offset if offset.present?
+      result
+    end
+  end
+
+  def decode_prefixed(params, prefix)
+    params.select{|k, v| /^#{prefix}/ =~ k}.extend(HashExtension).convert_keys{|k| k.sub(/^#{prefix}/, '')}
+  end
 
   def create_swf(swf_series, swf_data)
     swf = swf_series.swfs.create!
